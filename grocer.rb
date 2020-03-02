120068e8a1364c542efa97e8941be33671015e1c
Duncan Uruchima <duruchima1@gmail.com>
2020-03-02 21:51:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -33,26 +33,37 @@ def consolidate_cart(cart)
   new_cart
 end
 
-def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
-  coupons.each do |coupon|
-    coupon.each do |attribute, value|
-      name = coupon[:item]
-
-      if cart[name] && cart[name][:count] >= coupon[:num]
-        if cart["#{name} W/COUPON"]
-          cart["#{name} W/COUPON"][:count] += 1
-        else
-          cart["#{name} W/COUPON"] = {:price => coupon[:cost],
-          :clearance => cart[name][:clearance], :count => 1}
-        end
-
-      cart[name][:count] -= coupon[:num]
-    end
-  end
+def mk_coupon_hash(c)
+  rounded_unit_price = (c[:cost].to_f * 1.0 / c[:num]).round(2)
+  {
+    :item => "#{c[:item]} W/COUPON",
+    :price => rounded_unit_price,
+    :count => c[:num]
+  }
 end
+
+
+def apply_coupon_to_cart(matching_item, coupon, cart)
+  matching_item[:count] -= coupon[:num]
+  item_with_coupon = mk_coupon_hash(coupon)
+  item_with_coupon[:clearance] = matching_item[:clearance]
+  cart << item_with_coupon
+end
+
+def apply_coupons(cart, coupons)
+  i = 0
+  while i < coupons.count do
+    coupon = coupons[i]
+    item_with_coupon = find_item_by_name_in_collection(coupon[:item], cart)
+    item_is_in_basket = !!item_with_coupon
+    count_is_big_enough_to_apply = item_is_in_basket && item_with_coupon[:count] >= coupon[:num]
+
+    if item_is_in_basket and count_is_big_enough_to_apply
+      apply_coupon_to_cart(item_with_coupon, coupon, cart)
+    end
+    i += 1
+  end
+
   cart
 end
 
